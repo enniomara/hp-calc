1e63b8b409a89e55445938445eba854357fc6a02
Ennio Mara <enniomara@users.noreply.github.com>
2017-11-23 11:34:15 +0100

    Added the GUI used for testing.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void number(ActionEvent event) {
+    }
+
+    public void action(ActionEvent event) {
+    }
+
+    public void enter(ActionEvent event) {
+    }
 }
