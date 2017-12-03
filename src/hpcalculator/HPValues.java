8d01472efc95252c87a563fe421345fdc4838b29
chrillebile <chrillebile@gmail.com>
2017-12-03 21:35:27 +0100

    Added comments
    Added comments to methods in HPValues

--- src/hpcalculator/HPValues.java
+++ src/hpcalculator/HPValues.java
@@ -12,6 +12,11 @@ public class  HPValues{
         return values;
     }
 
+    /**
+     * Adding new values to values.
+     * @param value The value to be added.
+     * @return All the new values
+     */
     public Double[] push(Double value){
         if(values[0] == 0) {
             values[0] = value;
@@ -22,28 +27,46 @@ public class  HPValues{
         return getValues();
     }
 
+    /**
+     * Moving all the values to the right in the array.
+     */
     private void moveRight(){
         for (int i = 3; i > 0; i--){
             values[i] = values[i-1];
         }
     }
 
+    /**
+     * Moving all the values to the left in the array.
+     */
     private void moveLeft(){
         for(int i = 0; i < 3; i++){
             values[i] = values[i+1];
         }
     }
 
+    /**
+     * Getting the first value and removing the first from the array.
+     * @return The first value.
+     */
     public Double pop(){
         Double returnTemp = peek();
         moveLeft();
         return returnTemp;
     }
 
+    /**
+     * Looking at the first value.
+     * @return The first value.
+     */
     public Double peek(){
         return values[0];
     }
 
+    /**
+     * Resetting the array.
+     * @return The new values.
+     */
     public Double[] empty(){
         values = new Double[]{0.0, 0.0, 0.0, 0.0};
         return values;
