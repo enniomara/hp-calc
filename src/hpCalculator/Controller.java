7242822676aac1aa15b73f17d5199c4339833bc0
Ennio Mara <enniomara@users.noreply.github.com>
2017-12-08 16:47:29 +0100

    Added UI for the calculator and changed package to hpCalculator.
    Also changed Main.java to fit our application.

---
+++ src/hpCalculator/Controller.java
@@ -0,0 +1,35 @@
+package hpCalculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    private TextField textField_1;
+
+    @FXML
+    private TextField textField_2;
+
+    @FXML
+    private TextField textField_3;
+
+    @FXML
+    private TextField textField_4;
+
+    /**
+     * Handler for number and comma buttons.
+     * @param event The event.
+     */
+    @FXML
+    private void numberButtonHandler(ActionEvent event){
+    }
+
+    /**
+     * Handler for operations.
+     * @param event The event.
+     */
+    @FXML
+    private void operationButtonHandler(ActionEvent event){
+    }
+}
--- src/hpCalculator/Main.java
+++ src/hpCalculator/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package hpCalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,9 +10,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("hp-calculator.fxml"));
+        primaryStage.setTitle("HP-Calculator");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
